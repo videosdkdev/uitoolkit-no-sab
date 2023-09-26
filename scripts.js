24ed72a2cc02817e65be87fa1dcd1e9be7f10d52
tommygaessler <tommy.gaessler@zoom.us>
2023-09-26 13:23:07 -0600

    created join link and post session survey

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-// make API request after click join session
-// make inputs to set session name, session passcode, and username.
+const urlParams = new URLSearchParams(window.location.search);
+const preSetSession = urlParams.get('session');
+const preSetpasscode = urlParams.get('passcode');
+
+if(preSetSession) {
+    // set session in inputs
+    document.getElementById('sessionName').value = preSetSession
+}
+
+if(preSetpasscode) {
+    // set passcode in inputs
+    if(preSetpasscode.length > 10) {
+        document.getElementById('passcodeLength').style.display = 'block'
+    } else {
+        document.getElementById('sessionPasscode').value = preSetpasscode
+    }
+}
 
 var UIToolKitConfig = {
     features: ['video', 'audio', 'settings', 'users', 'share', 'chat'],
@@ -21,27 +36,42 @@ function closePreview() {
 }
 
 function getVideoSDKJWT() {
+
+    document.getElementById('nameRequired').style.display = 'none'
+    document.getElementById('sessionNameRequired').style.display = 'none'
+    document.getElementById('passcodeLength').style.display = 'none'
+    document.getElementById('rating').style.display = 'none'
     
     UIToolKitConfig.userName = document.getElementById('yourName').value
     UIToolKitConfig.sessionName = document.getElementById('sessionName').value
     UIToolKitConfig.sessionPasscode = document.getElementById('sessionPasscode').value
 
-    fetch('https://or116ttpz8.execute-api.us-west-1.amazonaws.com/default/videosdk', {
-        method: 'POST',
-        body: JSON.stringify({
-            sessionName:  UIToolKitConfig.sessionName,
-            role: 1,
-            telemetryTrackingId: `tommy-ui-toolkit-${UIToolKitConfig.sessionName}-${UIToolKitConfig.userName}-${Date.now()}`
+    if(UIToolKitConfig.userName && UIToolKitConfig.sessionName) {
+        fetch('https://or116ttpz8.execute-api.us-west-1.amazonaws.com/default/videosdk', {
+            method: 'POST',
+            body: JSON.stringify({
+                sessionName:  UIToolKitConfig.sessionName,
+                role: 1,
+                telemetryTrackingId: `tommy-ui-toolkit-${UIToolKitConfig.sessionName}-${UIToolKitConfig.userName}-${Date.now()}`
+            })
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data)
+            UIToolKitConfig.videoSDKJWT = data.signature
+            joinSession()
+        }).catch((error) => {
+            console.log(error)
         })
-    }).then((response) => {
-        return response.json()
-    }).then((data) => {
-        console.log(data)
-        UIToolKitConfig.videoSDKJWT = data.signature
-        joinSession()
-    }).catch((error) => {
-        console.log(error)
-    })
+    } else {
+        if(!UIToolKitConfig.userName) {
+            document.getElementById('nameRequired').style.display = 'block'
+        }
+
+        if(!UIToolKitConfig.sessionName) {
+            document.getElementById('sessionNameRequired').style.display = 'block'
+        }
+    }
 }
 
 function joinSession() {
@@ -62,6 +92,7 @@ function joinSession() {
     window.ZoomUIToolKit.subscribe("uitoolkit-destroy", () => {
         document.getElementById('header').style.display = 'block'
         document.getElementById('join-flow').style.display = 'block'
+        document.getElementById('rating').style.display = 'block'
     })
 }
 
@@ -69,4 +100,5 @@ function leaveSession() {
     window.ZoomUIToolKit.destroy();
     document.getElementById('header').style.display = 'block'
     document.getElementById('join-flow').style.display = 'block'
+    document.getElementById('rating').style.display = 'block'
 }
